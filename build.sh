03b30711923e83868f090c48d2e8d6d7b94db075
tomasz likus <tomasz.likus@bgch.co.uk>
2015-12-08 16:37:44 +0000

    fix build command, change dependency, bump version

--- build.sh
+++ build.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-fpm -s dir -t deb -n collectd-rabbitmq -f -v 0.0.1-1ubuntu -d collectd -a all --verbose --deb-priority optional --url https://github.com/ConnectedHomes/collectd-rabbitmq --description "Plugin for collectd stats monitoring of rabbitmq" -vendor "bgch devops <devops@bgch.co.uk>" --maintainer "bgch devops <devops@bgch.co.uk>" etc usr
+VER=0.0.1-3ubuntu
+
+fpm -s dir -t deb -n collectd-rabbitmq -f -v $VER -d bgch-collectd -a all --verbose --deb-priority optional --url "https://github.com/ConnectedHomes/collectd-rabbitmq" --description "Plugin for collectd stats monitoring of rabbitmq" --vendor "bgch devops <devops@bgch.co.uk>" --maintainer "bgch devops <devops@bgch.co.uk>" etc usr
